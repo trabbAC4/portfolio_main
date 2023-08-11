4df3d230c21c6b0399fee2f4b7f1e7d5bc56ac00
Tahmidur_Rabb <tr1476@nyu.edu>
2023-08-11 01:58:59 -0400

    cert section

---
+++ javascript/main.js
@@ -0,0 +1,35 @@
+
+function getRandomPosition(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function createStars() {
+    const container = document.getElementById("container3");
+    
+    for (let i = 0; i < 5; i++) {
+        const star = document.createElement("div");
+        star.classList.add("star");
+        star.style.left = `${getRandomPosition(0, container.clientWidth - 50)}px`;
+        star.style.top = `${getRandomPosition(0, container.clientHeight - 50)}px`;
+        star.innerHTML = "<h1><i class='fa-sharp fa-regular fa-star fa-2xs' style='color: #d9d9d9;'></i></h1>";
+        unlighted = star.innerHTML;
+
+        star.onclick = function() {
+            if (star.innerHTML == unlighted) {
+                star.innerHTML = "<h1><i class='fa-sharp fa-regular fa-star fa-2xs' style='color: #e8eb1e;'></i></h1>";
+        }
+            else {
+                star.innerHTML = unlighted;
+            }
+        };
+        container.appendChild(star);
+    }
+}
+
+window.onload = function() {
+    createStars();
+    draggableElements.forEach(element => {
+        element.style.left = getRandomPosition(0, 200) + "px";
+        element.style.top = getRandomPosition(0, 200) + "px";
+});
+};
